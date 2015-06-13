bd0ec143815ee983ebaa0c0b5183c49251cc5709
Luis Felipe L. M <luisfelipelmtc@gmail.com>
2015-06-13 19:41:02 +0000

    Rest 2

--- api/index.php
+++ api/index.php
@@ -8,19 +8,26 @@ $app->get('/', function() {
 });
 
 $app->get('/guests', function() use ($app) { // 'use' é usado para usar uma variavel dentro da funcao, neste caso $app
-	$guests = array(
-		array('id' => 1, 'name' => 'Luis Felipe', 'email' => 'luisfelipelmtc@gmail.com'),
-		array('id' => 2, 'name' => 'Cecilia', 'email' => 'ceciliaaserpa@gmail.com'),
-		array('id' => 3, 'name' => 'Alexandre', 'email' => 'alexandre05@gmail.com'),
-	);	
+	$db = getDB();
+	$guests = array();
+	foreach($db->guests() as $guest) {
+		$guests[] = array(
+			'id' => $guest['id'],
+			'name' => $guest['name'],
+			'email' => $guest['email']
+		);
+	}
 	
 	$app->response()->header('Content-Type', 'application/json');
 	echo json_encode($guests);
 });
 
 $app->post('/guest', function() use ($app){
-	$guest = json_decode($app->request->getBody(), true);
-	$guest['id'] = 10;
+	$db = getDB();
+
+	$guestToAdd = json_decode($app->request->getBody(), true);
+	$guest = $db->guests->insert($guestToAdd);
+	
 	$app->response->header('Content-Type', 'application/json');
 	echo json_encode($guest);
 });
@@ -28,6 +35,21 @@ $app->post('/guest', function() use ($app){
 $app->delete('/guest/:id', function ($id)  use ($app) {
 		echo $id;
 });
+
+function getConnection() {
+	$dbhost = getenv('IP');
+	$dbuser = getenv('C9_USER');
+	$dbpass = '';
+	$dbname = 'c9';
+	$pdo = new PDO("mysql:host=$dbhost;dbname=$dbname;", $dbuser, $dbpass);
+	return $pdo;
+}
+
+function getDB(){
+	$pdo = getConnection();
+	$db = new NotORM($pdo);
+	return $db;
+}
 
 $app->run();
 
